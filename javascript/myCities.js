e6ba483d1f5b1930b9c7beeb4d89fa8bf9d8caf6
anshu.agrawal <anshu.agrawal@lge.com>
2015-01-20 11:17:25 +0900

    Dynamic datalist creation and filling it with option value. Dynamic fetching of data also from myCities.js file

---
+++ javascript/myCities.js
@@ -0,0 +1,34 @@
+/*This is a JSON file for collection of cities.
+This file is used by DynamicDatalistSample*/
+var myCities = [
+	{
+		city: "Korba"
+	},
+	{
+		city: "Raipur"
+	},
+	{
+		city: "Bangalore"
+	},
+	{
+		city: "Bilaspur"
+	},
+	{
+		city: "Chennai"
+	},
+	{
+		city: "Durg"
+	},
+	{
+		city: "Bhilai"
+	},
+	{
+		city: "Raigarh"
+	},
+	{
+		city: "Champa"
+	},
+	{
+		city: "Dharamjaigarh"
+	}
+]
